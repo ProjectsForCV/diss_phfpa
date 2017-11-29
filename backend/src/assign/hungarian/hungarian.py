3c8a2db9f4ad84daeb94468e8b0bad2c73eda42e
Daniel <danielcooke1996@gmail.com>
2017-11-29 18:03:36 +0000

    added reduceCols, started drawLines method

--- backend/src/assign/hungarian/hungarian.py
+++ backend/src/assign/hungarian/hungarian.py
@@ -14,46 +14,32 @@ def minimise(m):
         raise MatrixError(m)
 
     npa = __createNumpyArray__(m)
-    assigned = False
-
-    #while(assigned == False):
-
-    __reduceRows__(npa)
-    assigned = __isAssigned__(npa)
 
 
-    print("Assigned :%a" % assigned)
-    print(npa)
+    log(npa, "Initial Matrix: ")
+    npa = __reduceRows__(npa)
+    log(npa, "Reduced Rows: ")
+    npa = __reduceCols__(npa)
+    log(npa, "Reduced Columns: ")
+
 
 
 
 """Returns the highest cost assignment solution for matrix m (n by n)"""
-
 def maximise(m):
     if(np.ndim(m) != 2):
         raise MatrixError(m)
 
-def __isAssigned__(m):
+def __isSolved__(m):
     # Checks if the matrix has been solved
-    ass = True
-    print(m)
-    nonZeroSet =[]
-    for row in m:
-        nonZeroSet.append(np.array(row.nonzero()[0]))
+    # TODO: This section is basically checking if the min number of lines covering zeroes = n
 
-    # If the set of non zeros is a different length than the original array it means there two rows that have a zero in the same place
-    if (len(np.unique(nonZeroSet, axis=0)) != len(nonZeroSet)):
-        ass = False
+    pass
 
-    nonZeroSet = []
-    for col in m.T:
-        nonZeroSet.append(np.array(col.nonzero()[0]))
-
-    # If the set of non zeros is a different length than the original array it means there two rows that have a zero in the same place
-    if (len(np.unique(nonZeroSet, axis=0)) != len(nonZeroSet)):
-        ass = False
-
-    return ass
+def log(matrix, message):
+    print("%s\n" %message)
+    print("%s\n" %matrix)
+    print("--------------------------")
 
 def __createNumpyArray__(m):
     return np.array(m)
@@ -67,12 +53,34 @@ def __reduceRows__(m):
 
 def __reduceCols__(m):
     # Reduces cols in matrix n by subtracting the smallest value in each col from every other value in the col.
+    cols = []
+    for i in m.T:
+        cols.append(i - min(i))
 
-    pass
+    m = np.array(cols).T
+    return m
+
+def __drawLines__(m):
+    # Marks the minimum amount of lines on the matrix that cross out a zero in every row/column
+    assignedColumns = []
+    assignedRows = []
+    for rowIndex in range(len(m)):
+#       Initialise array , assignedColumns = []
+#       Initialise array, assignedRows = []
+#       For Each Row in the Matrix
+#           For Each Col in the Row
+#               z = INDEX of next zero in row
+#               IF z is IN assignedColumns
+#                   continue
+#               ELSE
+#                   assignedRows.push(Index of this row)
+        for colIndex in range(len(m[rowIndex])):
+            z = m[rowIndex,colIndex].index(0)
 
 testData = [
     [1,2,3],
     [3,1,2],
     [1,3,2]
 ]
+
 minimise(testData)
