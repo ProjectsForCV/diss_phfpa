7d8dcab07fa3b07d0025cdb535427145dd64adb0
Daniel <danielcooke1996@gmail.com>
2017-12-01 18:30:22 +0000

    init commit of basic server

---
+++ backend/src/server/server.js
@@ -0,0 +1,24 @@
+/***********************************************************************
+ * Date: 01/12/2017
+ * Author: Daniel Cooke
+ ***********************************************************************/
+// DCOOKE 01/12/2017 - TODO: This will need to be hosted on digital ocean for testing purposes 
+const express = require('express');
+const app = express();
+const port = 4000;
+
+
+app.get('/', (request, response) => {
+
+    response.send("Hello World")
+});
+
+
+app.listen(port, (err) =>{
+
+    if (err) {
+        return console.log(err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
