0400e5a912bfaa50b6d46d7f9e2e07af0d9f8fbf
Daniel <danielcooke1996@gmail.com>
2018-01-11 19:18:22 +0000

    added digital ocean host to config, ready for demo

--- frontend/src/app/services/http/http-base-service.ts
+++ frontend/src/app/services/http/http-base-service.ts
@@ -8,7 +8,7 @@ export class HttpBaseService {
 
   public HOST_ENDPOINT;
   public MOCK_HOST = 'http://localhost:12345';
-  public REAL_HOST;
+  public REAL_HOST = 'http://178.62.107.105:12345';
 
   constructor(public http: Http) {
     this.HOST_ENDPOINT = Globals.RunMode === Globals.AppRunModeEnum.MOCK ? this.MOCK_HOST : this.REAL_HOST;
