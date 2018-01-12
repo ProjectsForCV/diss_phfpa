23e16774e56a4e1d91391476c5ba6e6ed7e74179
Daniel <danielcooke1996@gmail.com>
2018-01-12 14:59:02 +0000

    added CORS support

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -16,6 +16,20 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+
+
+});
+
 app.get('/api/randomMat/:rows/:cols', (request, response) => {
 
     const rows = request.params['rows'];
